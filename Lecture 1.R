61cdcc2eea974317aa9475ad789bc1ffc8d7096b
Ank221199 <ankitsinha.221199@gmail.com>
2020-07-23 11:26:21 +0530

    Add The R File
    The R File id from Lecture 1

---
+++ Lecture 1.R
@@ -0,0 +1,129 @@
+X <- 10
+X
+
+y <- 20
+y
+
+X+y
+X-y
+X*y
+X/y
+X^y
+X=5
+y=2
+X%%y
+X%/%y
+
+X<y
+X>y
+X<=y
+X>=y
+X==y
+X!=y
+!X
+X|y
+X&y
+isTRUE(X)
+
+X <- TRUE
+y <- FALSE
+!X
+X|y
+X&y
+isTRUE(y)
+isTRUE(X)
+class(X)
+typeof(X)
+
+X <- 10
+typeof(X)
+class(X)
+
+g <- 6.24
+g
+class(g)
+typeof(g)
+
+s <- as.integer(3)
+s
+S1 <- as.integer(3.9)
+S1
+
+typeof(S1)
+
+x <- 5i
+x^2
+
+k <- 1+2i
+k
+typeof(k)
+
+
+sqrt(-1)
+
+a <- 4 ; b <- 6
+g <- a > b
+g
+class(g)
+
+g <- as.character(69)
+g
+class(g)
+typeof(g)
+g <- "6969"
+g
+class(g)
+typeof(g)
+
+g <- as.integer(g)
+g
+typeof(g)
+class(g)
+
+test <- "Hello Duniya"
+test
+typeof(test)
+test <- as.integer(test)
+test
+typeof(test)
+
+
+test <- "22 22 22"
+test
+typeof(test)
+test <- as.integer(test)
+test
+typeof(test)
+
+test1 <- "Hello"
+test2 <- "Duniya"
+test3 <- test1 + test2
+
+test3 <- paste(test1,test2)
+test3
+
+
+test1 <- "1 1 1"
+test2 <- "2"
+test3 <- test1 + test2
+
+test1 <- "1"
+test2 <- "2"
+test3 <- as.integer(test1) + as.integer(test2)
+test3
+
+paste0(test1,test2)
+
+a <- c(12, 2, 5, 8, 9, 22, 11, 99)
+a
+typeof(a)
+class(a)
+
+b=10
+b
+class(b)
+
+
+c <-c("Ankit", "Arbind", "Sinha")
+class(c)
+typeof(c)
