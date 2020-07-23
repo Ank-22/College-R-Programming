1e405542e1ed4c9637863b5f216329894a8ada0f
Ank221199 <ankitsinha.221199@gmail.com>
2020-07-23 13:43:08 +0530

    Added Outputs

--- Session 2/Session 2.R
+++ Session 2/Session 2.R
@@ -10,30 +10,42 @@ Name
 
 a <- c(1, 2, 3, 6, -2, 4)
 class(a)
-
+# O :"numeric"
 a
+# 0 : [1]  1  2  3  6 -2  4
 
+# Number Vector
 
 X <-c(1,2,3)
+# O :
 typeof(X)
+# O :[1] "double"
 class(X)
+# O :[1] "numeric"
 str(X)
+# O :num [1:3] 1 2 3
+
+# Integer Vector
 
 x1 <-c(1L,2L,3L)
-
 typeof(x1)
-
+# O :[1] "integer"
 class(x1)
-
+# O :[1] "integer"
 length(x1)
-
+# O :[1] 3
 str(x1)
+# O :int [1:3] 1 2 3
 
+# Logical Vector
 
 y <- c(TRUE,FALSE,TRUE,FALSE,TRUE)
-
 typeof(y)
+# o:[1] "logical"
 class(y)
-
+# o:[1] "logical"
 length(y)
+# o:[1] "logical"
 str(y)
+# o:logi [1:5] TRUE FALSE TRUE FALSE TRUE
+
