e53aa7f631b256c40bb6ea73062618e8269edf0e
Ank221199 <ankitsinha.221199@gmail.com>
2020-07-23 13:26:40 +0530

    Added

--- Session 2/Session 2.R
+++ Session 2/Session 2.R
@@ -1,4 +1,11 @@
 #------------ Session 2 - Data Structure ------------#
 
+Name <- c("Ankit","Aizen","Ichigo","Naruto")
+Name
 # vector 
 # Numeric Vector
+
+a <- c(1, 2, 3, 6, -2, 4)
+class(a)
+a
+
