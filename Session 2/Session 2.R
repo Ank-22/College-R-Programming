5f647bbea391847d8a04533752feff5ab809cd20
Ank221199 <ankitsinha.221199@gmail.com>
2020-07-23 13:33:49 +0530

    Logical Vector

--- Session 2/Session 2.R
+++ Session 2/Session 2.R
@@ -2,10 +2,38 @@
 
 Name <- c("Ankit","Aizen","Ichigo","Naruto")
 Name
+# O: [1] "Ankit"  "Aizen"  "Ichigo" "Naruto"
+
+
 # vector 
 # Numeric Vector
 
 a <- c(1, 2, 3, 6, -2, 4)
 class(a)
+
 a
 
+
+X <-c(1,2,3)
+typeof(X)
+class(X)
+str(X)
+
+x1 <-c(1L,2L,3L)
+
+typeof(x1)
+
+class(x1)
+
+length(x1)
+
+str(x1)
+
+
+y <- c(TRUE,FALSE,TRUE,FALSE,TRUE)
+
+typeof(y)
+class(y)
+
+length(y)
+str(y)
