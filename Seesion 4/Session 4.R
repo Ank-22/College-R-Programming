697ea0847a10fee151f4054b69c81017b6057e1b
Ank221199 <ankitsinha.221199@gmail.com>
2020-08-06 13:28:47 +0530

    🐱‍🚀 Changing oders

--- Seesion 4/Session 4.R
+++ Seesion 4/Session 4.R
@@ -23,3 +23,18 @@ is.factor(factor_2)
 
 
 levels(factor_data)
+
+
+" 
+Changing the order of levels 
+The order of the levels in function again with new order of the level
+"
+
+# Applying the factoe function with required order of the level
+
+new_order <- factor(data, levels=c("East","West","North"))
+
+new_order
+
+str(new_order)
+is.factor(new_order)
