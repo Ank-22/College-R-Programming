78f34a46403a5fbf07246a5b2fd961bbd3eb071c
Ank221199 <ankitsinha.221199@gmail.com>
2020-08-06 13:11:05 +0530

    Created Session 4

---
+++ Seesion 4/Session 4.R
@@ -0,0 +1,11 @@
+## --------- SESSION 4 ---------##
+
+## Factor
+# Create a vector as input
+
+data <- c("East","West","East","North","North","East","West","West","East","North")
+data
+
+is.factor(data)
+str(data)
+
