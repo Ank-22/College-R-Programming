fd8bb1bf1af036843aea5a91669012cb350e2db7
Ank221199 <ankitsinha.221199@gmail.com>
2020-09-03 13:16:16 +0530

    🕸 Created Session 8.R

---
+++ Session 8/Session 8.R
@@ -0,0 +1,23 @@
+"
+Create a new file: EDA with dplyr.R
+
+Save the cominc.csv and copy paste
+
+change the path \ with /
+"
+
+# Exploratory Data Analysis with dplyr
+# Visualizations using ggplot
+
+# Install the below libaries only once the frist time
+# Load the libaries each time for a new R session 
+
+install.packages("readr")
+install.packages("ggplot2")
+install.packages("dplyr")
+
+library(readr)
+library(ggplot2)
+library(dplyr)
+
+# Now read in the data and explore the dataset
