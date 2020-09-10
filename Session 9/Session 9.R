1cb2687d13bbdccd80ad5206ae5315399387dbb3
Ank221199 <ankitsinha.221199@gmail.com>
2020-09-10 13:29:12 +0530

    🐴 Ploting Graph with categorically value

--- Session 9/Session 9.R
+++ Session 9/Session 9.R
@@ -3,3 +3,25 @@ library(ggplot2)
 library(dplyr)
 
 dataset_1 = read.csv("Session 9/cars04.csv" , stringsAsFactors=TRUE)
+#Last Session
+comics =read.csv("Session 8/comics.csv" , stringsAsFactors=TRUE)
+comics=comics%>%
+        filter(align !='Reformed Criminals')
+
+dim(comics)
+str(comics$align)
+levels(comics$align)
+table(comics$align)
+
+comics=comics%>%
+        filter(align !='Reformed Criminals')%>%
+        droplevels()
+
+table(comics$align)
+
+table(comics$align,comics$gender)
+
+ggplot(comics,aes(x=align, fill=gender))+geom_bar(position="dodge")+theme(axis.text.x=element_text(angle=90))
+
+
+ggplot(comics,aes(x=align, fill = align))+geom_bar()+ facet_wrap(~ gender)
